075a8e58eee2ffe1f551a4f87eb02b4b0201bed5
Nathan Falcone <ntfalcone93@gmail.com>
2016-08-02 14:59:34 -0600

    Added groups and some controllers

---
+++ Reso/Reso/FirebaseController.swift
@@ -0,0 +1,47 @@
+//
+//  FirebaseController.swift
+//  MyPets
+//
+//  Created by Nathan on 6/8/16.
+//  Copyright © 2016 Falcone Development. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+class FirebaseController {
+    
+    static let ref = FIRDatabase.database().reference()
+}
+
+protocol FirebaseType {
+    var endpoint: String {get}
+    var identifier: String? {get set}
+    var dictionaryCopy: [String: AnyObject] {get}
+    
+    init?(dictionary: [String: AnyObject], identifier: String)
+    
+    mutating func save()
+    func delete()
+}
+
+extension FirebaseType {
+    
+    mutating func save() {
+        var newEndpoint = FirebaseController.ref.child(endpoint)
+        if let identifier = identifier {
+            newEndpoint = newEndpoint.child(identifier)
+        } else {
+            newEndpoint = newEndpoint.childByAutoId()
+            self.identifier = newEndpoint.key
+        }
+        newEndpoint.updateChildValues(dictionaryCopy)
+    }
+    
+    func delete() {
+        guard let identifier = identifier else {
+            return
+        }
+        FirebaseController.ref.child(endpoint).child(identifier).removeValue()
+    }
+}
