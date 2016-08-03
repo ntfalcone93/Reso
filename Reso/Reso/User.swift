834b2c342fa3e0c6326ad1091b493bb8c4fc5c2b
patrickpahl <ppahl84@yahoo.com>
2016-08-03 15:23:20 -0600

    Finished model objects

--- Reso/Reso/User.swift
+++ Reso/Reso/User.swift
@@ -7,3 +7,49 @@
 //
 
 import Foundation
+import UIKit
+
+struct User: FirebaseType {
+    
+    private let kFirstName = "firstName"
+    private let kLastName = "lastName"
+    private let kFullName = "fullName"
+    private let kPhotoUrl = "photo"
+    
+    var firstName: String
+    var lastName: String
+    var fullName: String
+    var photoUrl: String
+    var identifier: String?
+    
+    var endpoint: String {
+        return "users"
+    }
+    
+    var dictionaryCopy: [String : AnyObject] {
+        return [kFirstName: firstName, kLastName: lastName, kFullName: fullName, kPhotoUrl: photoUrl]
+    }
+    
+    init(firstName: String, lastName: String, fullName: String, photoUrl: String, identifier: String) {
+        self.firstName = firstName
+        self.lastName = lastName
+        self.fullName = fullName
+        self.photoUrl = photoUrl
+        self.identifier = identifier
+    }
+    
+    
+    init?(dictionary: [String: AnyObject], identifier: String) {
+        guard let firstName = dictionary[kFirstName] as? String,
+            let lastName = dictionary[kLastName] as? String,
+            let fullName = dictionary[kFullName] as? String,
+            let photoUrl = dictionary[kPhotoUrl] as? String else {
+                return nil
+        }
+        self.firstName = firstName
+        self.lastName = lastName
+        self.fullName = fullName
+        self.photoUrl = photoUrl
+        self.identifier = identifier
+    }
+}
