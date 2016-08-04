f3722149588debee3f1a72f1701708182e72dded
Jonathan Parker Rogers <parker.rogers24@gmail.com>
2016-08-04 13:40:17 -0600

    login and main screen interface

---
+++ Reso/Reso/MainMenuTableViewCell.swift
@@ -0,0 +1,35 @@
+//
+//  MainMenuTableViewCell.swift
+//  Reso
+//
+//  Created by Jonathan Rogers on 8/3/16.
+//  Copyright © 2016 ResoPolling. All rights reserved.
+//
+
+import UIKit
+
+class MainMenuTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var votingStatusImageView: UIImageView!
+    
+    @IBOutlet weak var pollNameLabel: UILabel!
+    
+    @IBOutlet weak var membersIconImageView: UIImageView!
+    
+    @IBOutlet weak var numberOfMembersLabel: UILabel!
+    
+    @IBOutlet weak var timerRemainingLabel: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
