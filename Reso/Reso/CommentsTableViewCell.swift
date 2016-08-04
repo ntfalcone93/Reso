356f96f68c4424192a4b815aa58cc82598722709
SeanGilhuly <seangilhuly@hotmail.com>
2016-08-03 19:46:11 -0600

    Setup PollDetailVC and subclassed files
    Setup PollDetailVC and two view controllers associated with the two containter views.  Added a CommentsTVC and subclassed.

---
+++ Reso/Reso/CommentsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CommentsTableViewCell.swift
+//  Reso
+//
+//  Created by Sean Gilhuly on 8/3/16.
+//  Copyright © 2016 ResoPolling. All rights reserved.
+//
+
+import UIKit
+
+class CommentsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
