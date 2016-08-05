a822c518b270177781acc73666dad2007b7e21b0
patrickpahl <ppahl84@yahoo.com>
2016-08-05 10:54:52 -0600

    Setup PollController and made changes to user and user controller

--- Reso/Reso/PollController.swift
+++ Reso/Reso/PollController.swift
@@ -8,56 +8,14 @@
 
 import Foundation
 
-// TODO: Move to new file when ready
-/***********************************************/
-import UIKit
-
-class PollListViewController: UIViewController, UITableViewDataSource {
-    
-    @IBOutlet var tableView: UITableView!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(reloadTable), name: PollController.pollsChangedNotification, object: nil)
-    }
-    
-    func reloadTable() {
-        tableView.reloadData()
-    }
-    
-    // MARK: - Table view data source
-    
-    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return PollController.sharedController.polls.count
-    }
-    
-    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        // TODO: return real cell for poll
-        let cell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath)
-        let poll = PollController.sharedController.polls[indexPath.row]
-        print(poll) // TODO: Remove debug logging
-        // cell.update(with: poll)
-        return cell
-    }
-    
+enum PollType {
+    case Public
+    case Private
 }
-/***********************************************/
 
 class PollController {
     
-    static let sharedController = PollController()
-    
-    static let pollsChangedNotification = "pollAddedNotification"
-    
-    var polls = [Poll]()
-    var publicPolls: [Poll] {
-        return polls.filter { !$0.isPrivate }
-    }
-    var privatePolls: [Poll] {
-        return polls.filter { $0.isPrivate }
-    }
-    
-    func create(title: String, options: [Option], memberIds: [String], isPrivate: Bool, endDate: NSDate) {
+    static func create(title: String, options: [Option], memberIds: [String], isPrivate: Bool, endDate: NSDate) {
         var newPoll = Poll(title: title, options: options, memberIds: memberIds, isPrivate: isPrivate, endDate: endDate)
         newPoll.save()
     }
@@ -66,38 +24,63 @@ class PollController {
         poll.delete()
     }
     
-    /**
-     Subscribes to all poll objects in Firebase. Sends an `NSNotification` named `PollController.pollsChangedNotification` when
-     a poll is added, changed, or removed.
-     
-     Note: This only needs to be called once in the app—after the current user has authenticated, and you have the current user id.
-     */
-    func subscribeToAllPolls() {
-        let query = FirebaseController.ref.child("polls").queryOrderedByChild("members").queryEqualToValue(UserController.shared.currentUserId)
-        query.observeEventType(.ChildAdded, withBlock: { data in
-            guard let dataDict = data.value as? [String: AnyObject],
-                poll = Poll(dictionary: dataDict, identifier: data.key) else { return }
-            self.polls.append(poll)
-            NSNotificationCenter.defaultCenter().postNotificationName(PollController.pollsChangedNotification, object: data.key)
-        })
-        query.observeEventType(.ChildChanged, withBlock: { data in
-            guard let dataDict = data.value as? [String: AnyObject],
-                poll = Poll(dictionary: dataDict, identifier: data.key), index = self.polls.indexOf(poll) else { return }           ///Index?
-            self.polls[index] = poll
-            NSNotificationCenter.defaultCenter().postNotificationName(PollController.pollsChangedNotification, object: data.key)
-            // Using data.key as object allows you to subscribe to this notification only for a specific identifier:
-            // Example for detail page
-//            NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(update), name: PollController.pollsChangedNotification, object: currentPoll.identifier)
-        })
-        query.observeEventType(.ChildRemoved, withBlock: { data in
-            guard let dataDict = data.value as? [String: AnyObject],
-                poll = Poll(dictionary: dataDict, identifier: data.key), index = self.polls.indexOf(poll) else { return }
-            self.polls.removeAtIndex(index)
-            NSNotificationCenter.defaultCenter().postNotificationName(PollController.pollsChangedNotification, object: data.key)
+    
+    static func observePolls(pollType: PollType, completion: (polls: [Poll]) -> Void) {
+        switch pollType {
+        case .Private:
+            // We need to observe the poll that belong to the current user
+            let currentUserId = UserController.shared.currentUserId
+            let ref = FirebaseController.ref.child("usersPolls").child(currentUserId)
+            ref.observeEventType(.Value, withBlock: { (data) in
+                guard let pollDict = data.value as? [String: AnyObject] else {
+                    completion(polls: [])
+                    return
+                }
+                let pollIds = Array(pollDict.keys)
+                var polls = [Poll]()
+                let group = dispatch_group_create()
+                for pollId in pollIds {
+                    dispatch_group_enter(group)
+                    fetchPollWithIdentifier(pollId, completion: { (poll) in
+                        if let poll = poll {
+                            polls.append(poll)
+                            dispatch_group_leave(group)
+                        }
+                    })
+                }
+                dispatch_group_notify(group, dispatch_get_main_queue(), {
+                    completion(polls: polls)
+                })
+            })
+        case .Public:
+            // If its public then we will observe only the polls that are public
+            let ref = FirebaseController.ref.child("polls").queryOrderedByChild("isPrivate").queryEqualToValue(false)
+            ref.observeEventType(.Value, withBlock: { (data) in
+                guard let pollDicts = data.value as? [String: [String: AnyObject]] else {
+                    completion(polls: [])
+                    return
+                }
+                let polls = pollDicts.flatMap { Poll(dictionary: $1, identifier: $0) }
+                completion(polls: polls)
+            })
+        }
+    }
+    
+    
+    static func fetchPollWithIdentifier(identifier: String, completion: (poll: Poll?) -> Void) {
+        let ref = FirebaseController.ref.child("polls").child(identifier)
+        ref.observeSingleEventOfType(.Value, withBlock:  { (data) in
+            guard let pollDict = data.value as? [String: AnyObject] else {
+                completion(poll: nil)
+                return
+            }
+            let poll = Poll(dictionary: pollDict, identifier: identifier)
+            completion(poll: poll)
         })
     }
     
-    func vote(option: Option) {                                                                          ///ref poll identifier
+    
+    static func vote(option: Option) {                                                                          ///ref poll identifier
     
     
     
