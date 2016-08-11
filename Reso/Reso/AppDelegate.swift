8d7323785aadfc12be517c5671818698b8847065
Nathan Falcone <ntfalcone93@gmail.com>
2016-08-11 10:04:14 -0600

    Added app icons

--- Reso/Reso/AppDelegate.swift
+++ Reso/Reso/AppDelegate.swift
@@ -18,7 +18,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         
         FIRApp.configure()
-        UIApplication.sharedApplication().statusBarStyle = .LightContent
         
         return true
     }
